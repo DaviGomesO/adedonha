54fa6c76b474d53edca81d843927adeeee67e7e8
Davi Gomes <84361019+DaviGomesO@users.noreply.github.com>
2023-11-05 23:07:28 -0300

    Archive Initial

---
+++ SORTEADOR_ADEDONHA.py
@@ -0,0 +1,17 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+from time import sleep
+import random
+
+ALFABETO = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q','R','S', 'T', 'U','V', 'W','X','Y','Z']
+
+def sortearLetra():
+    while True:
+        for i in range(5,0,-1):
+            print(i)
+            sleep(1)
+        letra_sorteada = random.choice(ALFABETO)
+        return letra_sorteada
+        
+letra_sorteada = sortearLetra()
+print(f"A letra sorteada é: {letra_sorteada}")
